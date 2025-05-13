e5fac6f0ad3f6b4a5a9c76cad47aaa101d1b3d9c
eneas <eneas.eng@yahoo.com>
2025-05-13 20:22:06 -0300

    feat: adiciona estrutura dos microserviços

---
+++ src/test/java/com/eneas/sensors/device/manager/DeviceManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eneas.sensors.device.manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeviceManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
