e1f6d2298e076730d14750793159e7572953aca5
Enéas Almeida <eneas.eng@yahoo.com>
2025-06-03 08:33:07 -0300

    feat: implementa microservico monitoring

--- src/main/java/com/eneas/sensors/device/manager/domain/repositories/SensorRepository.java
+++ src/main/java/com/eneas/sensors/device/manager/domain/repositories/SensorRepository.java
@@ -2,7 +2,6 @@ package com.eneas.sensors.device.manager.domain.repositories;
 
 import com.eneas.sensors.device.manager.domain.models.Sensor;
 import com.eneas.sensors.device.manager.domain.models.SensorId;
-import io.hypersistence.tsid.TSID;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
